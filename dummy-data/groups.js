24588dbe6f4cfe54ad2ca3e8974c56874a2856e6
dizzlesticks <sodha.dhiran@gmail.com>
2025-01-13 13:26:10 +0000

    add groups dummy data

---
+++ dummy-data/groups.js
@@ -0,0 +1,53 @@
+export const groupData = [
+  {
+    _id: { $oid: "6784d715844f23ac9810cf28" },
+    group_name: "test_group1",
+    group_desc: "A group for testing purposes",
+    members: [
+      { $oid: "6784d64b844f23ac9810cf21" },
+      { $oid: "6784d64b844f23ac9810cf22" },
+      { $oid: "6784d64b844f23ac9810cf23" },
+    ],
+  },
+  {
+    _id: { $oid: "6784d715844f23ac9810cf29" },
+    group_name: "coding_club",
+    group_desc: "A group for coding enthusiasts",
+    members: [
+      { $oid: "6784d64b844f23ac9810cf24" },
+      { $oid: "6784d64b844f23ac9810cf25" },
+      { $oid: "6784d64b844f23ac9810cf26" },
+    ],
+  },
+  {
+    _id: { $oid: "6784d715844f23ac9810cf30" },
+    group_name: "retro_gaming_fans",
+    group_desc: "A group for fans of retro and arcade gaming",
+    members: [
+      { $oid: "6784d64b844f23ac9810cf27" },
+      { $oid: "6784d64b844f23ac9810cf28" },
+      { $oid: "6784d64b844f23ac9810cf29" },
+      { $oid: "6784d64b844f23ac9810cf30" },
+    ],
+  },
+  {
+    _id: { $oid: "6784d715844f23ac9810cf31" },
+    group_name: "music_buffs",
+    group_desc: "A group for people passionate about music",
+    members: [
+      { $oid: "6784d64b844f23ac9810cf22" },
+      { $oid: "6784d64b844f23ac9810cf26" },
+      { $oid: "6784d64b844f23ac9810cf28" },
+    ],
+  },
+  {
+    _id: { $oid: "6784d715844f23ac9810cf32" },
+    group_name: "book_club",
+    group_desc: "A group for book lovers to share and discuss",
+    members: [
+      { $oid: "6784d64b844f23ac9810cf21" },
+      { $oid: "6784d64b844f23ac9810cf23" },
+      { $oid: "6784d64b844f23ac9810cf27" },
+    ],
+  },
+];
