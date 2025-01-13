7ccb4d195bda976f3f321371547a4dad7ae2ba3a
dizzlesticks <sodha.dhiran@gmail.com>
2025-01-13 13:21:33 +0000

    add user dummy data

---
+++ dummy-data/users.js
@@ -0,0 +1,62 @@
+export const userData = [
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf21" },
+    username: "sparkle_unicorn",
+    name: "Sparkle Unicorn",
+    email: "sparkle@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf22" },
+    username: "pixel_penguin",
+    name: "Pixel Penguin",
+    email: "pixel@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf23" },
+    username: "cosmic_koala",
+    name: "Cosmic Koala",
+    email: "cosmic@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf24" },
+    username: "robo_raptor",
+    name: "Robo Raptor",
+    email: "robo@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf25" },
+    username: "ninja_narwhal",
+    name: "Ninja Narwhal",
+    email: "ninja@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf26" },
+    username: "galaxy_giraffe",
+    name: "Galaxy Giraffe",
+    email: "galaxy@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf27" },
+    username: "turbo_turtle",
+    name: "Turbo Turtle",
+    email: "turbo@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf28" },
+    username: "fluffy_fox",
+    name: "Fluffy Fox",
+    email: "fluffy@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf29" },
+    username: "jellybean_owl",
+    name: "Jellybean Owl",
+    email: "jellybean@testmail.com",
+  },
+  {
+    _id: { $oid: "6784d64b844f23ac9810cf30" },
+    username: "bouncy_bear",
+    name: "Bouncy Bear",
+    email: "bouncy@testmail.com",
+  },
+];
