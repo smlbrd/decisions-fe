2e1e733032a7e6d7db36d22f3ca6a81aa91e0b77
dizzlesticks <sodha.dhiran@gmail.com>
2025-01-14 12:17:13 +0000

    add modal to groups create new group and fix unique keys error

--- app/(tabs)/Groups.tsx
+++ app/(tabs)/Groups.tsx
@@ -1,30 +1,55 @@
 import { Collapsible } from "@/components/Collapsible";
 import { CreateNewButton } from "@/components/CreateNewButton";
-import React from "react";
+import React, { useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { groupData } from "../../dummy-data/groups";
 import UserCard from "@/components/UserCard";
+import Overlay from "@/components/Overlay";
 
 export default function Groups() {
+  const [isCreateGroupModalVisible, setIsCreateGroupModalVisible] =
+    useState(false);
+  const handleCreateGroupModalClose = () => {
+    setIsCreateGroupModalVisible(false);
+  };
+  const handleCreateNewGroupPress = () => {
+    setIsCreateGroupModalVisible(true);
+  };
   const usersById: { [key: string]: { username: string } } = {};
   // keys are strings, and so are values
   const groupDataCollapsibles = groupData.map((group) => {
     return (
-      <Collapsible title={group.group_name}>
+      <Collapsible key={group._id.$oid} title={group.group_name}>
         {group.members.map((member) => {
           if (!usersById[member.$oid])
             usersById[member.$oid] = { username: "steve" }; // would be an axios get the user info
-          return <UserCard username={usersById[member.$oid].username} />;
+          return (
+            <UserCard
+              key={member.$oid}
+              username={usersById[member.$oid].username}
+            />
+          );
         })}
       </Collapsible>
     );
   });
   return (
-    <View style={styles.container}>
-      <CreateNewButton text={"Create New Group"} onPress={() => {}} />
-      <Text style={styles.headerText}>My Groups</Text>
-      {groupDataCollapsibles}
-    </View>
+    <>
+      <Overlay
+        isVisible={isCreateGroupModalVisible}
+        onClose={handleCreateGroupModalClose}
+      >
+        <Text>This is the modal for creating a new group</Text>
+      </Overlay>
+      <View style={styles.container}>
+        <CreateNewButton
+          text={"Create New Group"}
+          onPress={handleCreateNewGroupPress}
+        />
+        <Text style={styles.headerText}>My Groups</Text>
+        {groupDataCollapsibles}
+      </View>
+    </>
   );
 }
 
