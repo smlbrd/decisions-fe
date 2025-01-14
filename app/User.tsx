a9fc41d98182905f1da822c80b1c88f14ffd458b
Zeineb <zeineb1mukhtar1@gmail.com>
2025-01-14 16:56:02 +0000

    add user profile changes

--- app/User.tsx
+++ app/User.tsx
@@ -1,9 +1,59 @@
-import { Text, View } from "react-native";
+import React, {useEffect, useState} from "react"
+import { Text, View, Button, Image } from "react-native";
+import UserInformation from '../components/UserInformation'
+import {apiClient} from '../utils/api-client'
 
 export default function User() {
+  const userId = 1 
+  const userIdInStorage = 1
+
+  const [userData, setUserData] = useState<any>(null)
+  const [loading, setLoading] = useState<boolean>(true)
+  const [error, setError] = useState<string | null>(null)
+
+  useEffect(() => {
+    apiClient.get(`/users/${userId}`).then(({data}) => {
+      setUserData(data)
+      setLoading(false)
+    }).catch((err) => {
+      setError("Error loading user data")
+      setLoading(false)
+    })
+  }, [userId])
+
+  const isUsersOwnProfile = userId === userIdInStorage
+
+  const canSeeEmail = isUsersOwnProfile
+  const canEditLists = isUsersOwnProfile
+
+  // const hasPermission = userId === userIdInStorage
+  const handleLogout = () => {
+    console.log("Logging Out")
+  }
+
+  if (loading) {
+    return <Text>Loading ..</Text>
+  }
+
+  if (error) {
+    return <Text>{error}</Text>
+  }
+
   return (
-    <View>
-      <Text>My Profile</Text>
-    </View>
-  );
-}
+    <View style={{padding: 10}}>
+    <Text style={{fontSize: 20, marginBottom: 20}}>My Profile</Text>
+
+    {userData && (
+      <UserInformation
+      userId={userData._id} username={userData.username} email={userData.email} canSeeEmail={canSeeEmail} canEditLists={canEditLists} /> )}
+    {/* userIdInStorage={userIdInStorage} /> */}
+
+    {isUsersOwnProfile && (<Button title="Log out" onPress={handleLogout} />)}
+  </View>
+);
+} 
+
+
+
+
+
---
+++ components/UserInformation.tsx
@@ -0,0 +1,39 @@
+import React from "react";
+import { Text, View, Image, Button } from "react-native";
+// import { userData } from "../dummy-data/users";
+
+type UserInformationProps = {
+  userId: number;
+  canSeeEmail: boolean;
+  canEditLists: boolean;
+  username: string;
+  email?: string;
+};
+
+const UserInformation: React.FC<UserInformationProps> = ({
+  userId,
+  canSeeEmail,
+  canEditLists,
+  username,
+  email,
+}) => {
+
+  return (
+    <View>
+      {/* Profile picture */}
+      {userId && (
+        <Image source={{ uri: `${userId}` }} style={{ width: 100, height: 100, borderRadius: 50 }} />
+      )}
+      <Text style={{ fontSize: 20, fontWeight: "bold" }}>{username}</Text>
+      <Text>@{username}</Text>
+
+      {canSeeEmail && email && <Text>Email: {email}</Text>}
+
+      {canEditLists && <Text> Edit your Lists here</Text>}
+    </View>
+  );
+  // }: UserInformationProps) {
+  //   const user = userData.find((user) => user._id === userId.toString());
+};
+
+export default UserInformation;
