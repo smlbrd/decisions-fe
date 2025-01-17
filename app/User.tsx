f93c7e575504b2cea41eb72849656e7d0429b32c
Zeineb <zeineb1mukhtar1@gmail.com>
2025-01-17 16:47:33 +0000

    using user context instead of axios

--- app/User.tsx
+++ app/User.tsx
@@ -1,67 +1,42 @@
 import React, {useEffect, useState} from "react"
 import { Text, View, Button, Image } from "react-native";
 import UserInformation from "../components/UserInformation"
-import apiClient from '../utils/api-client'
 import {TouchableOpacity} from 'react-native' //instead of button for logging out
 import {ActivityIndicator} from 'react-native' // instead of text, spinner to show its loading
 import {useRouter} from 'expo-router'
+import { useUser } from "@/utils/UserContext";
 
 
 export default function User() {
-  //hardcoded for now, but this may change if doing user authentification 
-  const userId = "6784d64b844f23ac9810cf21"
-  const userIdInStorage = "6784d64b844f23ac9810cf21"
-
-  const [userData, setUserData] = useState<any>(null)
-  const [isLoading, setLoading] = useState<boolean>(true)
-  const [error, setError] = useState<string | null>(null)
-
-  const router = useRouter()
+    const {user} = useUser()
+    console.log(user)
+    const [isLoading, setLoading] = useState<boolean>(true)
+//   const [error, setError] = useState<string | null>(null)
+    const router = useRouter()
 
   useEffect(() => {
-    apiClient.get(`/users/${userId}`).then(({data}) => {
-      console.log(data)
-      setUserData(data)
-      setLoading(false)
-    }).catch((err) => { 
-      console.error("Error loading user data", err)
-      setError("Error loading user data")
-      setLoading(false)
-    })
-  }, [userId])
+     setLoading(!user._id)
+    }, [user])
 
-  const isUsersOwnProfile = userId === userIdInStorage
-
-  const canSeeEmail = isUsersOwnProfile
-  const canEditLists = isUsersOwnProfile
-
-  // const hasPermission = userId === userIdInStorage
-  const handleLogout = () => {
+    if (isLoading || !user._id) {
+        return ( <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
+          <ActivityIndicator size="large" color="#0000ff" /> </View>
+        )
+      }
+      
+      const handleLogout = () => {
     console.log("Logging Out")
     router.push("/")
-  }
- 
-
-  if (isLoading) {
-    return ( <View style={{flex:1, justifyContent: 'center', alignItems: 'center'}}>
-      <ActivityIndicator size="large" color="#0000ff" /> </View>
-    )
-  }
-
-  if (error) {
-    return <Text>{error}</Text>
   }
 
   return (
     <View style={{padding: 10}}>
     <Text style={{fontSize: 30, marginBottom: 20}}>My Profile</Text>
 
-    {userData && (
-      <UserInformation
-      userId={userData._id} username={userData.username} email={userData.email} canSeeEmail={canSeeEmail} canEditLists={canEditLists} /> )}
-    {/* userIdInStorage={userIdInStorage} /> */}
+    {/* {user && ( */}
+    <UserInformation user={user}/>
 
-    {isUsersOwnProfile && (
+    {user._id && (
       <TouchableOpacity onPress={handleLogout}
       style={{backgroundColor: '#ff4d4d',
         padding: 10,
--- components/UserInformation.tsx
+++ components/UserInformation.tsx
@@ -1,35 +1,35 @@
 import React from "react";
 import { Text, View, Image, Button } from "react-native";
-import { userData } from "../dummy-data/users";
 
 type UserInformationProps = {
-  userId: string;
-  canSeeEmail: boolean;
-  canEditLists: boolean;
-  username: string;
-  email?: string;
+    user: {
+  _id: string | null;
+  username: Array<string> | null
+  name: string | null;
+  email: string | null;
+  savedLists: Array<string> | null;
+    }
 };
 
 const UserInformation: React.FC<UserInformationProps> = ({
-  userId,
-  canSeeEmail,
-  canEditLists,
-  username,
-  email,
+  user,
 }) => {
+    const {username, email, savedLists} = user
 
   return (
     <View>
       {/* Profile picture */}
-      {userId && (
+      {user._id && (
         <Image source={require('../assets/images/sparkly_unicorn.png')} style={{ width: 100, height: 100, borderRadius: 50 }} />
       )}
       <Text style={{ fontSize: 20, fontWeight: "bold" }}>{username}</Text>
       <Text>@{username}</Text>
 
-      {canSeeEmail && email && <Text>Email: {email}</Text>}
+      {email && <Text>Email: {email}</Text>}
 
-      {canEditLists && <Text> Edit your Lists here</Text>}
+      {savedLists && savedLists.length > 0 && (
+        <Text> Saved Lists here: {savedLists.join(", ")}</Text>
+      )}
     </View>
   );
   // }: UserInformationProps) {
