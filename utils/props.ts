d5ade45b181730dda353a44b1b74ba27298c8793
dizzlesticks <sodha.dhiran@gmail.com>
2025-01-20 14:22:39 +0000

    create props file

---
+++ utils/props.ts
@@ -0,0 +1,45 @@
+export interface OptionProps {
+  _id: string | null;
+  name: string | null;
+  description: string | null;
+  customFields: string[];
+  owner: string | null;
+  createdAt: string | null;
+  __v: number | null;
+}
+export interface ListProps {
+  _id: string | null;
+  title: string | null;
+  description: string | null;
+  options: OptionProps[];
+  owner: string | null;
+  members: string[];
+  createdAt: string | null;
+  __v: number | null;
+}
+export interface GroupProps {
+  _id: string | null;
+  title: string | null;
+  description: string | null;
+  options: OptionProps[];
+  owner: string[];
+  members: string[];
+}
+export interface DecisionProps {
+  _id: string | null;
+  votingStatus: string | null;
+  decisionsProcess_id: string | null;
+  completedAt: string | null;
+  outcome: string | null;
+  __v: number | null;
+  createdAt: string | null;
+  updatedAt: string | null;
+}
+export interface UserProps {
+  _id: string | null;
+  username: string | null;
+  name: string | null;
+  email: string | null;
+  savedLists: Array<string> | null;
+  avatarImg?: string | null;
+}
