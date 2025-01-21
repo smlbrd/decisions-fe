affc6fc55be07cf92ff4d92460b6ca6ea4fb3dfc
dizzlesticks <sodha.dhiran@gmail.com>
2025-01-21 10:54:33 +0000

    make decision process without user turns or socket

--- components/decision-processes/ThisOrThat.tsx
+++ components/decision-processes/ThisOrThat.tsx
@@ -5,6 +5,7 @@ import apiClient from "@/utils/api-client";
 import { useState } from "react";
 import shuffleArray from "../../utils/shuffleArray";
 import getTwoRandomElements from "../../utils/getTwoRandomElements";
+import { ThisOrThatButton } from "../ThisOrThatButton";
 
 type Props = {
   decisionData: DecisionProps;
@@ -49,6 +50,61 @@ export default function ThisOrThat({ decisionData, setDecisionData }: Props) {
         console.log(err);
       });
   };
+
+  const handleChoice = (thisOrThat: number) => {
+    const flip = thisOrThat === 1 ? 0 : 1;
+    const newRemainingOptions = decisionData.saveData.remainingOptions.filter(
+      (remainingOption) => {
+        return (
+          remainingOption._id !== decisionData.saveData.currentOptions[flip]._id
+        );
+      }
+    );
+    const newVoteHistory = [
+      ...decisionData.saveData.voteHistory,
+      decisionData.saveData.currentOptions[thisOrThat],
+    ];
+    const newTurnNumber = decisionData.saveData.turnNumber + 1;
+    if (newRemainingOptions.length === 1)
+      apiClient
+        .put(`decisions/${decisionData._id}`, {
+          votingStatus: "completed",
+          saveData: {
+            turnNumber: null,
+            playerOrder: null,
+            remainingOptions: newRemainingOptions,
+            currentOptions: null,
+            voteHistory: newVoteHistory,
+          },
+          outcome: newRemainingOptions[0],
+        })
+        .then(({ data }) => {
+          console.log(data);
+          setDecisionData(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    else {
+      const newCurrentOptions = getTwoRandomElements(newRemainingOptions);
+      apiClient
+        .put(`decisions/${decisionData._id}`, {
+          saveData: {
+            turnNumber: newTurnNumber,
+            remainingOptions: newRemainingOptions,
+            currentOptions: newCurrentOptions,
+            voteHistory: newVoteHistory,
+          },
+        })
+        .then(({ data }) => {
+          setDecisionData(data);
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    }
+  };
+
   return isStartingDecision ? (
     <Text>starting decision...</Text>
   ) : startDecisionErrMsg ? (
@@ -64,7 +120,24 @@ export default function ThisOrThat({ decisionData, setDecisionData }: Props) {
         </View>
       ) : decisionData.votingStatus === "in progress" ? (
         <View style={styles.decisionProcessContainer}>
-          <Text>you started the decision!</Text>
+          <Text>CHOOOOOSE</Text>
+          <ThisOrThatButton
+            onPress={handleChoice}
+            thisOrThat={0}
+            text={decisionData.saveData.currentOptions[0].name}
+            image_url={decisionData.saveData.currentOptions[0].image_url}
+          />
+          <ThisOrThatButton
+            onPress={handleChoice}
+            thisOrThat={1}
+            text={decisionData.saveData.currentOptions[1].name}
+            image_url={decisionData.saveData.currentOptions[1].image_url}
+          />
+        </View>
+      ) : decisionData.votingStatus === "completed" ? (
+        <View>
+          <Text>AND YOUR DECISION IS.........</Text>
+          <Text>{decisionData.saveData.remainingOptions[0].name}</Text>
         </View>
       ) : null}
     </View>
--- utils/props.ts
+++ utils/props.ts
@@ -1,11 +1,11 @@
 export interface OptionProps {
   _id: string | null;
-  name: string | null;
+  name: string;
   description: string | null;
   customFields: string[];
   owner: string | null;
   createdAt: string | null;
-  image_url?: string | null;
+  image_url?: string;
   __v: number | null;
 }
 export interface ListProps {
@@ -26,17 +26,25 @@ export interface GroupProps {
   owner: string[];
   members: string[];
 }
+export interface SaveDataThisOrThatProps {
+  turnNumber: number;
+  turnOrder: UserProps[];
+  remainingOptions: OptionProps[];
+  currentOptions: [OptionProps, OptionProps];
+  voteHistory: OptionProps[];
+}
 export interface DecisionProps {
   _id: string | null;
   votingStatus: string | null;
   decisionsProcess_id: string;
   completedAt: string | null;
-  outcome: string | null;
+  outcome: OptionProps | null;
   __v: number | null;
   createdAt: string | null;
   updatedAt: string | null;
   group: GroupProps | null;
   list: ListProps | null;
+  saveData: SaveDataThisOrThatProps;
 }
 export interface UserProps {
   _id: string | null;
